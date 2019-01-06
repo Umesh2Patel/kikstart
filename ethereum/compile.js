d1cbb66e92c4e092eba93383d30764c4b735e101
Umesh <umesh.khunt@gmail.com>
2019-01-05 17:44:16 -0800

    Add git ignore file

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -1,15 +1,15 @@
 const path = require('path');
-const fs = require('fs-extra');
-const solc = require('solc');
+const fs = require('fs-extra'); //File System
+const solc = require('solc'); // Solidity compile
 
-const buildPath = path.resolve(__dirname, 'build');
-fs.removeSync(buildPath);
+const buildPath = path.resolve(__dirname, 'build'); //path.resolve will construct the path for different OS
+fs.removeSync(buildPath); //removeSync deletes everything in the /build dir
 
 const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
 const source = fs.readFileSync(campaignPath, 'utf8');
 const output = solc.compile(source, 1).contracts;
 
-fs.ensureDirSync(buildPath);
+fs.ensureDirSync(buildPath); // ensureDirSync checks if dir exist if not then create the /build dir
 
 for(let contract in output) {
   fs.outputJsonSync(
